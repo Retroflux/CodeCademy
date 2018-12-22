9fc2e7f4a07b65840e851e7409afe6d15c1ecf08
Retroflux <jlange@uoguelph.ca>
2018-12-21 21:08:01 -0500

    Final touches

--- CSPathProject3-SalsShipping/salsShipping.py
+++ CSPathProject3-SalsShipping/salsShipping.py
@@ -1,9 +1,10 @@
 
 premiumGroundShipping = 125.00
 
-#calculates the ground shipping cost based on the weight of the package
+#calculates the ground shipping cost based on the weight given.
 def groundShippingPriceCalculator (weight):
-    cost = 20
+    cost = 20 #base cost
+    #test increasing weights to reduce comparisons to one per statement instead of boundary checks
     if weight <= 2:
         cost += (1.50*weight)
     elif weight <=6:
@@ -14,9 +15,10 @@ def groundShippingPriceCalculator (weight):
         cost += (4.75*weight)
     return cost
 
-
+#calculates the drone shipping costs based on the weight given.
 def droneShippingPriceCalculator (weight):
     cost = 0
+    #test increasing weights to reduce comparisons to one per statement instead of boundary checks
     if weight <= 2:
         cost += (4.50*weight)
     elif weight <=6:
@@ -29,11 +31,16 @@ def droneShippingPriceCalculator (weight):
 
 #This function will compare the values of the calculator functions
 def calculateLowestCost(weight):
+    
+    #call the above functions to get the numbers to compare
     costOfGroundShipping = groundShippingPriceCalculator(weight)
     costOfDroneShipping = droneShippingPriceCalculator(weight)
+
+    #output to ensure correctness
     print("Ground shipping: " + str(costOfGroundShipping))
     print("Drone shipping: " + str(costOfDroneShipping))
 
+    #final output based on a conditional branch
     print("For a package that weighs " + str(weight) + "...")
     if premiumGroundShipping < costOfDroneShipping and premiumGroundShipping < costOfGroundShipping:
         print("Premium ground shipping is the cheapest option and costs 125 dollars.")
@@ -42,11 +49,13 @@ def calculateLowestCost(weight):
     else:
         print("Drone shipping is the cheapest option and will cost " + str(costOfDroneShipping) + " dollars.")
 
-#Test values for the function
-        
+#Main#
+#Test values for the function  
 calculateLowestCost(2)
 print
 calculateLowestCost(100)
+print
+calculateLowestCost(41.5)
 print
 calculateLowestCost(4.8)
 print
